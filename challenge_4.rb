e018564c0eb0edcfbd547c865f0481cf01391217
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:18:26 -0700

    Add code to accomplish challenge

--- challenge_4.rb
+++ challenge_4.rb
@@ -2,8 +2,13 @@
 
 #Challenge in My Words: Go through the elements of an array and print the words that have the string "ing".
 #Pseudo code:
-# 1. Set up code so that I can look at each element of the array
-# 2. Use a method to scan through the string and find the characters "ing"
+# 1. Set up code so that I can look at each element of the array-- each method
+# 2. Use a method to scan through the string and find the characters "ing" -- include?() method
 # 3. Use puts to print the elements with "ing" to the terminal
 
+verbs = ["jumping", "run", "skipping", "eat", "playing"]
+
+verbs.each do |verb|
+    puts verb if verb.include?("ing")
+end 
 
