65adaf3d8fd293b0cb92e5546e96188b2cbc7dce
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:58:43 -0700

    Change formatting for readability

--- challenge_4.rb
+++ challenge_4.rb
@@ -1,7 +1,10 @@
-# Challenge: Start with an array of strings. Print only the words that include the letter combination "ing".
+# Challenge
+# Start with an array of strings. Print only the words that include the letter combination "ing".
 
-#Challenge in My Words: Go through the elements of an array and print the words that have the string "ing".
-#Pseudo code:
+#Challenge in My Words: 
+# Go through the elements of an array and print the words that have the string "ing".
+
+#Pseudocode
 # 1. Set up code so that I can look at each element of the array-- each method
 # 2. Use a method to scan through the string and find the characters "ing" -- include?() method
 # 3. Use puts to print the elements with "ing" to the terminal
