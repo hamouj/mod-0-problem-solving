ecaaa649a5eb1c7b8dcdc10dbea3fb2fe07bb38f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:21:47 -0700

    Define and add pseudocode for challenge

---
+++ challenge_5.rb
@@ -0,0 +1,10 @@
+# Start with an array of travel destinations. Print every travel destination in alphabetical order embedded in a sentence using string interpolation. For example, if the destination is "New York City", print something like "The next place I want to visit is New York City!"
+
+# Challenge in My Words:
+# Alphabetize each element of an array and then interpolate into a sentence.
+
+# Pseudocode:
+# 1. Put the elements in alphabetical order using a method --sort
+# 2. Run through each element of the array
+# 3. Print a sentence for each element
+
