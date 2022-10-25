406ba1b1e1a790183f0a7e95a731ddfa094b3c8e
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:16:15 -0700

    Define and add pseudocode for challenge

---
+++ challenge_4.rb
@@ -0,0 +1,9 @@
+# Challenge: Start with an array of strings. Print only the words that include the letter combination "ing".
+
+#Challenge in My Words: Go through the elements of an array and print the words that have the string "ing".
+#Pseudo code:
+# 1. Set up code so that I can look at each element of the array
+# 2. Use a method to scan through the string and find the characters "ing"
+# 3. Use puts to print the elements with "ing" to the terminal
+
+
