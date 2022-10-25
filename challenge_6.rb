2a9b39b6d007a30f239f28ebb2a8e1a78ad4acdf
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:58:28 -0700

    Add code to accomplish challenge

--- challenge_6.rb
+++ challenge_6.rb
@@ -4,6 +4,19 @@
 # In a sentence, change the beginning of each word to a capital letter.
 
 # Pseudocode:
-# 1. Write the sentence as an array.
+# 1. Break the sentence into words--create an array?
 # 2. Run each word (element) and capitalize the beginning of each word--capitalize
 # 3. Print each element as a sentence (in one line)--print not puts
+
+sentence = "turing is the best!"
+sentence.split.each do |sent|
+    print "#{sent.capitalize} "
+end 
+
+# OR
+
+sentence = "turing is the best!"
+array_words = sentence.split
+array_words.each do |array_word|
+    print array_word.capitalize, " "
+end 
