b9e023620f11d8b6bef469517741a70ce199c594
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-24 19:34:18 -0700

    Define and add pseudocode for challenge

---
+++ challenge_6.rb
@@ -0,0 +1,9 @@
+#Challenge: Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
+
+# Challenge in My Words:
+# In a sentence, change the beginning of each word to a capital letter.
+
+# Pseudocode:
+# 1. Write the sentence as an array.
+# 2. Run each word (element) and capitalize the beginning of each word--capitalize
+# 3. Print each element as a sentence (in one line)--print not puts
